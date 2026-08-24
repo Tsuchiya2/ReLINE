7a44fbd6406037848c4be0f26ffa8ae34bd59589
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2026-08-24 17:08:38 +0900

    refactor: 非推奨のconfig.cache_classesをconfig.enable_reloadingへ置き換える
    config.cache_classesはRails 8.1で非推奨のため、後継のconfig.enable_reloading
    へ移行する。真偽が反転するため、意味が変わらないよう1対1で対応させた。
    
    - development: cache_classes = false -> enable_reloading = true
    - test:        cache_classes = false -> enable_reloading = true
    - production:  cache_classes = true  -> enable_reloading = false
    
    development / testでRails.autoloaders.main.reloading_enabled?が従来どおり
    trueであることを確認済み。

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,7 +8,7 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = false
+  config.enable_reloading = true
   config.action_view.cache_template_loading = true
 
   # Do not eager load code on boot. This avoids loading your whole application
